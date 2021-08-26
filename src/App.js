0bb3d780ff8a64bf84f8b012a9d7312e60373f94
a-sambhab <saisambhav21chaini@gmail.com>
2021-08-27 00:25:58 +0530

    Added Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
-
-const App = () => {
+import React, {useState} from 'react';
+import { Info } from './Info';
+import './App.css'
+// import { Button } from '@material-ui/core';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+import AddBoxRoundedIcon from '@material-ui/icons/AddBoxRounded';const App = () => {
+  const [Icon, setIcon] = useState(AddBoxRoundedIcon);
+  const [state, setState] = useState(false);
+  const Change = () => {
+    // var element = document.getElementsById(id);
+    if(Icon===AddBoxRoundedIcon){
+      setIcon(IndeterminateCheckBoxIcon);
+      // setState(true);
+    }
+    else{
+      setIcon(AddBoxRoundedIcon);
+      // setState(false);
+    }
+  }
   return(
     <>
-      <h1>Hello</h1>
+      <div className="container">
+        <div className="centerdiv">
+          <h1>React Interview Questions</h1>
+          <div className="content">
+            {Info.map(function cards(val){
+              return(
+                <>
+                <div className="cards">
+                  <div className="show">
+                    <button onClick={() => setState(!state)} onClick={Change}><Icon/></button>
+                      <h3>{ val.question}</h3>
+                    </div>
+                  {state && <p className="ans">{val.answer}</p> }
+                </div>
+                </>
+              );
+            })
+
+            }
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/Info.jsx
@@ -0,0 +1,17 @@
+export const Info = [
+    {
+        id: 1,
+        question: 'What is a virtual Dom?',
+        answer: "DOM stand for Document Object Model. "
+    },
+    {
+        id: 2,
+        question: 'What is a virtual Dom?',
+        answer: "DOM stand for Document Object Model. "
+    },
+    {
+        id: 3,
+        question: 'What is a virtual Dom?',
+        answer: "DOM stand for Document Object Model. "
+    }
+]
